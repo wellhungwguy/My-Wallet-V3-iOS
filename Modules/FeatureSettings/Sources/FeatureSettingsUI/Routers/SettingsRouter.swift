cdd78dd4b170c5742155348cfab3529b61cf7ddd
Oliver Atkinson <88668746+oliveratkinson-bc@users.noreply.github.com>
2022-07-06 18:06:55 +0100

    Update SettingsRouter.swift

--- Modules/FeatureSettings/Sources/FeatureSettingsUI/Routers/SettingsRouter.swift
+++ Modules/FeatureSettings/Sources/FeatureSettingsUI/Routers/SettingsRouter.swift
@@ -463,7 +463,7 @@ final class SettingsRouter: SettingsRouterAPI {
         let interactor = SelectionScreenInteractor(service: selectionService)
         let presenter = SelectionScreenPresenter(
             title: LocalizationConstants.Settings.SelectCurrency.title,
-            description:  LocalizationConstants.Settings.SelectCurrency.description,
+            description: LocalizationConstants.Settings.SelectCurrency.description,
             searchBarPlaceholder: LocalizationConstants.Settings.SelectCurrency.searchBarPlaceholder,
             interactor: interactor
         )
