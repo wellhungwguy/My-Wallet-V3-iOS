9f0f6a0c6243cee2eb113938a49a5aad00dbb7f8
Alex McGregor <41585563+thisisalexmcgregor@users.noreply.github.com>
2022-08-11 04:40:52 -0500

    feat(Cards): IOS-6981 - Hint now shows while user is typing for credit cards (#4422)

--- Modules/Platform/Sources/PlatformUIKit/Views/TextField/TextFieldType.swift
+++ Modules/Platform/Sources/PlatformUIKit/Views/TextField/TextFieldType.swift
@@ -215,7 +215,6 @@ extension TextFieldType {
              .cardCVV,
              .expirationDate,
              .cardholderName,
-             .cardNumber,
              .description,
              .memo:
             return false
@@ -224,6 +223,7 @@ extension TextFieldType {
              .newPassword,
              .confirmNewPassword,
              .oneTimeCode,
+             .cardNumber,
              .cryptoAddress:
             return true
         }
