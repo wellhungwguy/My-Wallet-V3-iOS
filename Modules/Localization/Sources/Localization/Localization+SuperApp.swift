42be4eb47128a85868bd002881ac9c3073188baa
audrea-bc <102520557+audrea-bc@users.noreply.github.com>
2022-08-26 14:24:22 +0200

    fix(SuperApp): IOS-0000 Fix icons and typo  (#4495)
    ## Objective
    
    Fixing icons and typo

--- Modules/Localization/Sources/Localization/Localization+SuperApp.swift
+++ Modules/Localization/Sources/Localization/Localization+SuperApp.swift
@@ -29,7 +29,7 @@ extension LocalizationConstants {
 
 extension LocalizationConstants.AppMode {
     public static let privateKeyWallet = NSLocalizedString(
-        "Private Key Walelt",
+        "Private Key Wallet",
         comment: "Private Key Wallet"
     )
 
