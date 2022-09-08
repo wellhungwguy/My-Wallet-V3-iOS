12319168bebf9f26a4e5c7249f0af4bbf7ac3a23
dchatzieleftheriou-bc <71259279+dchatzieleftheriou-bc@users.noreply.github.com>
2022-09-08 15:42:10 +0300

    feature(Superapp): IOS-7015 Adds Polygon/Ethereum tags on Defi Mode (#4545)
    ## Objective
    
    Assets on DeFi mode (Private Key Wallet) should display their network
    
    ## Description
    
    This adds a tag, [Polygon/Ethereum] next to an asset on DeFi mode.
    
    ## How to Test
    
    - Build & Run
    - Ensure you have an account with DeFi assets on multiple networks
    - Verify that on Private Key Wallet mode, you can see the relevant
    network tag
    
    ## Screenshot
    
    <img width="534" alt="Screenshot 2022-09-05 at 14 38 37"
    src="https://user-images.githubusercontent.com/71259279/188440657-477cb6a4-d1e3-460a-a778-de433566534e.png">
    
    ## Merge Checklist
    
    - [ ] The PR uses a title supported by
    [.changelogrc](https://github.com/blockchain/My-Wallet-V3-iOS/blob/master/.changelogrc).
    - [ ] Code is formatted using `sh scripts/format-code.sh`.
    - [ ] UI components have accessibility identifiers and labels to support
    automated testing.
    - [ ] You have added unit tests if applicable.
    - [ ] You have added sufficient documentation (descriptive comments), if
    applicable.
    - [ ] If a user facing feature, you have posted a Loom to the
    appropriate Slack channels.
    
    Signed-off-by: dchatzieleftheriou-bc <dchatzieleftheriou@blockchain.com>

--- Modules/FeatureDashboard/Sources/FeatureDashboardUI/Portfolio/PortfolioViewController.swift
+++ Modules/FeatureDashboard/Sources/FeatureDashboardUI/Portfolio/PortfolioViewController.swift
@@ -18,6 +18,7 @@ import ToolKit
 import UIComponentsKit
 
 /// A view controller that displays the dashboard
+// swiftlint:disable type_body_length
 final class PortfolioViewController<OnboardingChecklist: View>: BaseScreenViewController {
 
     // MARK: - Private Types
@@ -141,7 +142,7 @@ final class PortfolioViewController<OnboardingChecklist: View>: BaseScreenViewCo
         tableView.register(FiatCustodialBalancesTableViewCell.self)
         tableView.registerNibCell(TotalBalanceTableViewCell.self, in: .module)
         tableView.registerNibCell(HistoricalBalanceTableViewCell.self, in: .module)
-        tableView.registerNibCell(SimpleBalanceTableViewCell.self, in: .module)
+        tableView.register(SimpleBalanceTableViewCell.self)
         tableView.register(HostingTableViewCell<WithdrawalLocksView>.self)
         tableView.separatorColor = .clear
 
