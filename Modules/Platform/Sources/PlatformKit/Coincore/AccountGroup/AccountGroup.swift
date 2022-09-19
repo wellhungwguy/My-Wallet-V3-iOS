35ea6a83663e974b8ced4158469ef77303d2fa5f
audrea-bc <102520557+audrea-bc@users.noreply.github.com>
2022-09-19 16:56:52 +0200

    fix(superapp): IOS-000 fix for total balance bug (#4604)
    ## Objective
    Same fix applied to 9.2, cherry picked to Dev

--- Modules/Platform/Sources/PlatformKit/Coincore/AccountGroup/AccountGroup.swift
+++ Modules/Platform/Sources/PlatformKit/Coincore/AccountGroup/AccountGroup.swift
@@ -88,6 +88,36 @@ extension AccountGroup {
         return type
     }
 
+    public func fiatBalance(fiatCurrency: FiatCurrency, at time: PriceTime) -> AnyPublisher<MoneyValue, Error> {
+        guard accounts.isNotEmpty else {
+            let logService: LogMessageServiceAPI = DIKit.resolve()
+            logService.logError(
+                message: "No accounts error - \(#function)",
+                properties: [
+                    "currency": fiatCurrency.code,
+                    "time": time.timestamp ?? ""
+                ]
+            )
+            return .failure(AccountGroupError.noAccounts)
+        }
+        return accounts
+            .chunks(ofCount: 100)
+            .map { accounts in
+                accounts
+                    .map { account in
+                        account.fiatBalance(fiatCurrency: fiatCurrency, at: time)
+                            .replaceError(with: MoneyValue.zero(currency: fiatCurrency))
+                    }
+                    .zip()
+            }
+            .zip()
+            .tryMap { (balances: [[MoneyValue]]) -> MoneyValue in
+                try balances.flatMap { $0 }
+                    .reduce(MoneyValue.zero(currency: fiatCurrency), +)
+            }
+            .eraseToAnyPublisher()
+    }
+
     public func balancePair(
         fiatCurrency: FiatCurrency,
         at time: PriceTime
@@ -95,7 +125,7 @@ extension AccountGroup {
         guard accounts.isNotEmpty else {
             let logService: LogMessageServiceAPI = DIKit.resolve()
             logService.logError(
-                message: "No accounts error",
+                message: "No accounts error - \(#function)",
                 properties: [
                     "currency": fiatCurrency.code,
                     "time": time.timestamp ?? ""
